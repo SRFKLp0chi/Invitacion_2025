304ee99ad4a5769d2dda56f8e3c8399340460c6d
Pochi1! <anaisbecerra18@gmail.com>
2025-02-28 02:50:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const audio = document.getElementById("audio.mp3");
-const playPauseBtn = document.getElementById("playPauseBtn");
-
-playPauseBtn.addEventListener("click", function() {
-    if (audio.paused) {
-        audio.play();
-        playPauseBtn.textContent = "⏸ Pause";
-    } else {
-        audio.pause();
-        playPauseBtn.textContent = "▶ Play";
-    }
-});
+// nada x ahora
